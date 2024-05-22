8e1581ac2d5b1b75ff62b606769db56254878e30
saleskit24 <saleskit@aitekph.com>
2024-05-23 01:06:32 +0800

    Update neets.js

--- models/neets.js
+++ models/neets.js
@@ -3,9 +3,9 @@ const axios = require('axios')
 async function neets(text){
     return axios.post('https://api.neets.ai/v1/tts', {
         text: text,
-        voice_id: 'us-female-4',
+        voice_id: 'kevin-hart',
         params: {
-            model: 'style-diff-500'
+            model: 'ar-diff-50k'
         },
     },
         {
